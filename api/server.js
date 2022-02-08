835504d24f9d9d6d615ef20c959c52d9d81656d3
Markeisha <markeisha94@gmail.com>
2022-02-07 21:21:46 -0600

    all test passing

--- api/server.js
+++ api/server.js
@@ -1,42 +1,84 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
-const User = require('./users/model');
-
+const userModel = require('./users/model');
 const server = express();
 
+server.use(express.json());
+
 server.get('/api/users', (req, res) => {
-    User.find()
+    userModel.find()
         .then(users => {
-            res.json(users)
+            res.json(users);
         })
-        .catch(err => {
-            res.status(500).json({
-                message: 'problem getting users',
-                stack: err.stack,
-                err: err.message
-            })
+        .catch(() => {
+            res.status(500).json({message: 'The users information could not be retrieved'});
         })
-})
+});
 
 server.get('/api/users/:id', (req, res) => {
-    User.findById(req.params.id)
+    let { id } = req.params;
+    userModel.findById(id)
         .then(user => {
-            res.json(user)
+            if(user == null) {
+                res.status(404).json({message: `The user with the specified ID does not exist`});
+            } else {
+                res.json(user);
+            }
         })
-        .catch(err => {
-            res.status(500).json({
-                message: 'problem getting users',
-                stack: err.stack,
-                err: err.message
+        .catch(() => {
+            res.status(500).json({message: `The user information could not be retrieved`});
+        })
+});
+
+server.post('/api/users', (req, res) => {
+    let user = req.body;
+    if(!user.name || !user.bio ){
+        res.status(400).json({message: 'Please provide name and bio for the user'});
+    } else {
+        userModel.insert(user)
+            .then(user => {
+                res.status(201).json(user);
             })
+            .catch(() => {
+                res.status(500).json({message: 'There was an error while saving the user to the database'});
+            })
+    }
+});
+
+server.put('/api/users/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await userModel.findById(id);
+        if(user == null) {
+            res.status(404).json({message: `The user with the specified ID does not exist`});
+            return;
+        }
+        let body = req.body;
+        if(!body.name || !body.bio) {
+            res.status(400).json({message: `Please provide name and bio for the user`});
+            return;
+        } else {
+            const newUser = await userModel.update(id, body);
+            res.status(200).json(newUser);
+        }
+    } catch (e) {
+        res.status(500).json({message: `The user information could not be modified`});
+    }
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    userModel.remove(id)
+        .then(user => {
+            if(user == null) {
+                res.status(404).json({message: `The user with the specified ID does not exist`});
+                return;
+            }
+            res.status(200).json(user);
         })
-})
-
-
-server.use('*', (req, res) => {
-    res.status(404).json({
-        message: 'not found'
-    })
-})
+        .catch(() => {
+            res.status(500).json({message: `The user could not be removed`});
+        })
+});
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
