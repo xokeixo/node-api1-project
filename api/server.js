b1a48d97f210bc33c4c21a6872d382550e57a24e
Markeisha <markeisha94@gmail.com>
2022-02-07 20:55:31 -0600

    finished up server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,38 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
+const User = require('./users/model');
 
 const server = express();
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'problem getting users',
+                stack: err.stack,
+                err: err.message
+            })
+        })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            res.json(user)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'problem getting users',
+                stack: err.stack,
+                err: err.message
+            })
+        })
+})
+
+
 server.use('*', (req, res) => {
     res.status(404).json({
         message: 'not found'
